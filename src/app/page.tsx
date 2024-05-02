1ae0815cf69e7aa83e17863693f26cf485eb82fe
Chrissie Canino <chrissie.canino@zwift.com>
2024-05-02 16:00:58 -0700

    fdfasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-5xl font-display font-bold">Hello World</h1>
+      <h1 className="text-5xl font-display font-bold">Hello World!!</h1>
     </main>
   );
 }
