3d7cd1e7939769956f52e2d72b5b570fb609013a
Chrissie Canino <chrissie.canino@zwift.com>
2024-05-02 15:00:37 -0700

    latest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import clsx from 'clsx';
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito',
+})
+
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito-sans',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={clsx(nunito.variable, nunitoSans.variable)}>
+      <body>{children}</body>
     </html>
   );
 }
