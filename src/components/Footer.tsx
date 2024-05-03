6c512e68d739aaeda8f07ce16afb1e0841045dad
Chrissie Canino <chrissie.canino@zwift.com>
2024-05-03 10:22:17 -0700

    added Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,22 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Logo from "@/components/Logo";
+import Bounded from "@/components/Bounded";
 export default async function Footer() {
     const client = createClient();
     const settings = await client.getSingle("settings");
-    return <footer>
-        <Link href="/">{settings.data.site_title}</Link>
-                <p>©{new Date ().getFullYear()} {settings.data.site_title}</p>
-                <ul>
-                    {settings.data.navigation.map(({link, label})=>(
-                        <li key={label}>
-                            <PrismicNextLink field = {link}>{label}</PrismicNextLink>
-                        </li>
-                    ))}
-                </ul>
-    </footer>
+    return <Bounded as ="footer" >
+        <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+            <Link href="/"><Logo /></Link>
+            <p className="text-xs">©{new Date ().getFullYear()} {settings.data.site_title}</p>
+            <ul className="flex">
+                {settings.data.navigation.map(({link, label})=>(
+                    <li key={label}>
+                            <PrismicNextLink field = {link} className="p-3">{label}</PrismicNextLink>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </Bounded>
 }
