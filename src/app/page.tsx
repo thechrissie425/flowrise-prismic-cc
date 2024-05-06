d80ecf5a2a20eb123d8ff2a778a916811b08c13b
Chrissie Canino <chrissie.canino@zwift.com>
2024-05-06 09:02:22 -0700

    update page.tsx in app/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,22 @@
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
-type Params = { uid: string };
-
-export const dynamicParams = false;
-
-export default async function Page({ params }: { params: Params }) {
+export default async function Page() {
   const client = createClient();
-  const page = await client
-    .getByUID("page", params.uid)
-    .catch(() => notFound());
+  const page = await client.getSingle("homepage");
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const page = await client.getByUID("page", params.uid);
+  const page = await client.getSingle("homepage");
 
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
   };
-}
-
-export async function generateStaticParams() {
-  const client = createClient();
-  const pages = await client.getAllByType("page");
-
-  return pages.map((page) => {
-    return { uid: page.uid };
-  });
 }
